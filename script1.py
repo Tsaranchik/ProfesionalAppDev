bbe1f855930eaf8dd77e0fa58ef79709cefe09a4
Vladimir Vedin <vovavedin123@gmail.com>
2024-03-18 12:06:59 +0400

    Commit from cloned repo

--- script1.py
+++ script1.py
@@ -1,6 +1,1 @@
-import random
-list = random.sample(range(0, 1001), 20)
-print(list)
-print(max(list))
-print("Goodbye!")
-#Тут есть какой-то код, этот комментарией является его имитацией
+print("Hello from cloned repo!")
