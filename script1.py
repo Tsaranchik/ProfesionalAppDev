3aff406e928d1acac8ef082cca36bae1f3c7643a
Vladimir Vedin <vovavedin123@gmail.com>
2024-03-18 11:50:15 +0400

    NEW FUNCTION IN SCRIPT1

--- script1.py
+++ script1.py
@@ -1,5 +1,6 @@
 import random
 list = random.sample(range(0, 101), 10)
 print(list)
-print(min(list))
+print(max(list))
+print("Goodbye!")
 #Тут есть какой-то код, этот комментарией является его имитацией
--- script2.py
+++
@@ -1,6 +0,0 @@
-import random
-list = random.sample(range(0, 101), 10)
-print(list)
-print(max(list))
-print("Thats all!")
-#Этот комментарий является имитацией кода
