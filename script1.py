5b73ec93520ea6e52e983dbab96f9ff372ec2e70
Vladimir Vedin <vovavedin123@gmail.com>
2024-03-18 12:04:43 +0400

    Changes in original repo

--- script1.py
+++ script1.py
@@ -1,6 +1,4 @@
-import random
-list = random.sample(range(0, 101), 10)
-print(list)
-print(min(list))
-print("Thats all!!! Googbye!!!")
-#Тут есть какой-то код, эgitтот комментарией является его имитацией
+def greet(name):
+    print(f"Hello, {name}!")
+
+greet("World")
