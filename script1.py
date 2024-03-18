9878c2dd684eedb14c2350980a3b63bdfb7a36d2
Vladimir Vedin <vovavedin123@gmail.com>
2024-03-18 11:45:39 +0400

    New function in script2

--- script1.py
+++ script1.py
@@ -2,4 +2,5 @@ import random
 list = random.sample(range(0, 101), 10)
 print(list)
 print(min(list))
+print("Thats all!!! Googbye!!!")
 #Тут есть какой-то код, этот комментарией является его имитацией
